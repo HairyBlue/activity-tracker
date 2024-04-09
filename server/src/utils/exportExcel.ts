83739704dc80fdab2e670e8e93ab35921c5b045e
Hairy Blue <pecision.nickimarty@gmail.com>
2024-04-09 10:20:05 +0800

    add sheet for num activities

--- server/src/utils/exportExcel.ts
+++ server/src/utils/exportExcel.ts
@@ -49,6 +49,35 @@ async function appendData(category: string[], club: any, year: any, semester: an
    return data
 }
 
+async function allActivies(category: string[], clubs: any, year: any, semester: any) {
+    let fdata: any[]= []
+    for (let club of clubs) {
+        const clubCon = club.clubAcronym.length > 0 ? `${club.clubName} (${club.clubAcronym})` : club.clubName
+        let data: any[] = [clubCon]
+        const result: any = await show(`SELECT Count(*) as count, categoryName from Activity INNER JOIN Club ON club_id = clubId INNER JOIN Category ON category_id = categoryId WHERE clubId = ? AND YEAR(activityStartDateIso) = ${year} AND activitySemester = ${semester} GROUP BY categoryName`,
+        [club.clubId]);
+    
+           
+        for (let ct of category) {
+            let hasCount = false
+            for (let res of result) {
+                if (ct == res.categoryName) {
+                    data.push(res.count)
+                    hasCount = true
+                }
+            }
+            if (!hasCount) {
+                data.push(null)
+            }
+        }
+
+        fdata.push(data)
+    }
+
+
+    return fdata
+}
+
 router.get("/excel-export", async function (req, res) {
     const { year, semester, filename = "records" } = req.query;
     
@@ -56,20 +85,33 @@ router.get("/excel-export", async function (req, res) {
     const clubs: any = await clubList()
 
     const workbook = XLSX.utils.book_new();
+    const COL_WIDTH = 200;
+
+    const ws2: any = XLSX.utils.aoa_to_sheet([[null]]);
+    for (let i = 0; i < category.length + 1; i++){
+        if(!ws2["!cols"]) ws2["!cols"] = [];
+        if(!ws2["!cols"][i]) ws2["!cols"][i] = {wch: 8};
+        ws2["!cols"][i].wpx = COL_WIDTH;
+    }
+
+    XLSX.utils.sheet_add_aoa(ws2, [category], { origin: "B1" });
+    const dataSheetActivities: any[] = await allActivies(category, clubs, year, semester)
+    XLSX.utils.sheet_add_aoa(ws2, dataSheetActivities, { origin: "A3" });
+    XLSX.utils.book_append_sheet(workbook, ws2, "NUMBER OF ACTIVITIES DONE");
 
     for (let club of clubs) {
         const ws: any = XLSX.utils.aoa_to_sheet([[club.clubName]]);
-        const COL_WIDTH = 200;
+       
         for (let i = 0; i < category.length + 1; i++){
-        if(!ws["!cols"]) ws["!cols"] = [];
-        if(!ws["!cols"][i]) ws["!cols"][i] = {wch: 8};
+            if(!ws["!cols"]) ws["!cols"] = [];
+            if(!ws["!cols"][i]) ws["!cols"][i] = {wch: 8};
             ws["!cols"][i].wpx = COL_WIDTH;
         }
 
         XLSX.utils.sheet_add_aoa(ws, [category], { origin: "B3" });
 
-        const data: any[] = await appendData(category, club, year, semester)
-        XLSX.utils.sheet_add_aoa(ws, data, { origin: "A4" });
+        const dataSheetClubs: any[] = await appendData(category, club, year, semester)
+        XLSX.utils.sheet_add_aoa(ws, dataSheetClubs, { origin: "A4" });
 
         const sheetName: string = club.clubAcronym.length > 0 ? club.clubAcronym: club.clubName.substring(0, 31)
         const newSheeet = sheetName.split("/").join("-")
